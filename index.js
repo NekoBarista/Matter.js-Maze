6678962b7e99353aa4240b003648fc12cdc8493c
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-07-13 22:52:33 +1000

    maze generation complete

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const width = 600;
 const height = 600;
 const cells = 3;
 
+const unitLength = width / cells;
+
 
 const render = Render.create({
     element: document.body,
@@ -45,26 +47,127 @@ Composite.add(world, walls);
 
 // maze generation
 
-const grid = Array(cells).fill(null).map(()=> Array(cells).fill(false));
+const shuffle = arr => {
+    let counter = arr.length;
+  
+    while (counter > 0) {
+      const index = Math.floor(Math.random() * counter);
+  
+      counter--;
+  
+      const temp = arr[counter];
+      arr[counter] = arr[index];
+      arr[index] = temp;
+    }
+  
+    return arr;
+  };
+  
+  const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+  
+  const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+  
+  const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+  
+  const startRow = Math.floor(Math.random() * cells);
+  const startColumn = Math.floor(Math.random() * cells);
+  
+  const drawMaze = (row, column) => {
+    // If i have visted the cell then return
+    if (grid[row][column]) {
+      return;
+    }
+  
+    // Mark this cell as  visited
+    grid[row][column] = true;
+  
+    // Assemble randomly-ordered list of neighbors
+    const neighbors = shuffle([
+      [row - 1, column, 'up'],
+      [row, column + 1, 'right'],
+      [row + 1, column, 'down'],
+      [row, column - 1, 'left']
+    ]);
+    // For each neighbor....
+    for (let neighbor of neighbors) {
+      const [nextRow, nextColumn, direction] = neighbor;
+  
+      // See if out of bounds
+      if (
+        nextRow < 0 ||
+        nextRow >= cells ||
+        nextColumn < 0 ||
+        nextColumn >= cells
+      ) {
+        continue;
+      }
+  
+      // If we have visited that neighbor, continue to next neighbor
+      if (grid[nextRow][nextColumn]) {
+        continue;
+      }
+  
+      // Remove a wall from either horizontals or verticals
+      if (direction === 'left') {
+        verticals[row][column - 1] = true;
+      } else if (direction === 'right') {
+        verticals[row][column] = true;
+      } else if (direction === 'up') {
+        horizontals[row - 1][column] = true;
+      } else if (direction === 'down') {
+        horizontals[row][column] = true;
+      }
+  
+      drawMaze(nextRow, nextColumn);
+    }
+  
+    // Go to next cell
+  };
+  
+  drawMaze(startRow, startColumn);
 
+  horizontals.forEach((row, rowIndex) => { 
+  row.forEach((open, columnIndex) => {
+    if (open) {
+        return;
+    }
+    
+    const wall = Bodies.rectangle(
+        columnIndex * unitLength + unitLength / 2,
+        rowIndex * unitLength + unitLength, unitLength, 10, {
+        isStatic: true }
+    );
+    
+    Composite.add(world, wall)
+    
+      });
 
-const verticals = Array(cells).fill(null).map(()=> Array(cells - 1).fill(false));
+    });
 
-const horizontals = Array(cells - 1).fill(null).map(()=> Array(cells).fill(false));
-
-const startRow =  Math.floor(Math.random()*cells)
-const startColumn =  Math.floor(Math.random()*cells)
-
-const cellMovement =  (row, startColumn) => {
-// if I have visited cell at row, column, then return
-// Mark this cell as visited
-// Assemble randomly ordered list of neighbours
-// For each neighbour....
-// Check if in bounds
-//Check if visited, continue to next
-// Remove wall from horizontal or vertical
-// Visit next cell
-
+    verticals.forEach((row, rowIndex) => {
+row.forEach((open, columnIndex) => {
+if (open) {
+    return
 }
 
-cellMovement(startRow, startColumn)
+const wall = Bodies.rectangle(
+columnIndex * unitLength + unitLength,
+rowIndex * unitLength + unitLength/2,
+10,
+unitLength, {
+    isStatic: true
+}
+
+
+);
+Composite.add(world, wall);
+
+    });
+   
+})
